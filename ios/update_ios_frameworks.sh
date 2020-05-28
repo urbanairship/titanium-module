66f03728e3e19c196f918ff8444a1f514c90606c
Marc Sciglimpaglia <marc@urbanairship.com>
2020-05-27 17:28:54 -0700

    [MOBILE-1604] fix carthage path

--- ios/update_ios_frameworks.sh
+++ ios/update_ios_frameworks.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+SCRIPT_DIR=`dirname "$0"`
+
 echo "Updating carthage"
 carthage update
 
 echo "Copying frameworks"
-cp -R Carthage/Build/iOS/AirshipCore.framework platform/
-cp -R Carthage/Build/iOS/AirshipMessageCenter.framework platform/
+cp -R "${SCRIPT_DIR}/Carthage/Build/iOS/AirshipCore.framework" "${SCRIPT_DIR}/platform/"
+cp -R "${SCRIPT_DIR}/Carthage/Build/iOS/AirshipMessageCenter.framework" "${SCRIPT_DIR}/platform/"
 
 echo "Done"
