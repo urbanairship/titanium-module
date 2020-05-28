b42d463d0625ebe8a9c0d9daf474f54e924e0441
Ryan Lepinski <ryan.lepinski@urbanairship.com>
2020-05-28 10:08:03 -0700

    Fix carthage copy

--- scripts/build_ios.sh
+++ scripts/build_ios.sh
@@ -7,14 +7,13 @@ echo "Building iOS"
 
 cd $ROOT_PATH/ios
 
-SCRIPT_DIR=`dirname "$0"`
 
 echo "Updating carthage"
 carthage update
 
 echo "Copying frameworks"
-cp -R "${SCRIPT_DIR}/Carthage/Build/iOS/AirshipCore.framework" "${SCRIPT_DIR}/platform/"
-cp -R "${SCRIPT_DIR}/Carthage/Build/iOS/AirshipMessageCenter.framework" "${SCRIPT_DIR}/platform/"
+cp -R "Carthage/Build/iOS/AirshipCore.framework" "platform/"
+cp -R "Carthage/Build/iOS/AirshipMessageCenter.framework" "platform/"
 
 echo "Building iOS"
 npx appc run -p ios --build-only
