bd45bd6f3c854b3babbb2e2aac0152537416cfc2
Ulrich Giberné <ulrich.giberne@airship.com>
2021-01-28 13:39:03 -0400

    fix check

--- scripts/check_titanium_version.sh
+++ scripts/check_titanium_version.sh
@@ -4,7 +4,7 @@ ROOT_PATH=`dirname "${0}"`/..
 
 TITANIUM_VERSION_EXPECTED=$(awk -F ' = ' "\$1 == \"TITANIUM_SDK_VERSION\" { print \$2 }" $ROOT_PATH/ios/titanium.xcconfig)
 
-appc ti sdk | grep "\[selected\]" > titanium_version.temp
+npx appc ti sdk | grep "\[selected\]" > titanium_version.temp
 ACTUAL_TITANIUM_VERSION=$(awk -F '[' "{ gsub(/ /, \"\"); print \$1 }" titanium_version.temp)
 rm titanium_version.temp
 
